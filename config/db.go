7c82a0a68c29ad41f4804a55f57f82d45697b482
northes <me@northes.io>
2023-05-22 18:33:15 +0800

    feat(config): DB DSN builder

--- config/db.go
+++ config/db.go
@@ -1,13 +1,17 @@
 package config
 
+import "fmt"
+
 type DBDriverType string
 
 var DBDriver = struct {
-	MySQL  DBDriverType
-	SQLite DBDriverType
+	MySQL   DBDriverType
+	SQLite  DBDriverType
+	SQLite3 DBDriverType
 }{
-	MySQL:  "mysql",
-	SQLite: "sqlite",
+	MySQL:   "mysql",
+	SQLite:  "sqlite",
+	SQLite3: "sqlite3",
 }
 
 type DB struct {
@@ -26,4 +30,18 @@ type MySQL struct {
 
 type SQLite struct {
 	Name string `mapstructure:"name"`
+}
+
+func (m *MySQL) GetDSN() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		m.User,
+		m.Password,
+		m.Host,
+		m.Port,
+		m.DBName,
+	)
+}
+
+func (s *SQLite) GetDSN() string {
+	return fmt.Sprintf("file:%s?cache=shared&_fk=1", s.Name)
 }
